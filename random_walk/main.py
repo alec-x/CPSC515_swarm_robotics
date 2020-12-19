d4d7fd7728cc7a5273f04ee87be14d8336aa1946
Alec Xu <Alec_Xu@hotmail.com>
2020-12-18 22:22:20 -0800

    add borders to world init

--- random_walk/main.py
+++ random_walk/main.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 from robot import robot
+from scipy.ndimage.filters import gaussian_filter
+from random import random
 
 num_bots = 10 # number of robots
-init_spacing = 50 # initial spacing (mm)
+s_range = 10 # range of wall detection
 
 obs_len = 100 # obstacle dimensions
 obs_width = 50
@@ -13,34 +15,45 @@ obs_width = 50
 world_x = 550 # occupancy grid with 1 mm resolution
 world_y = 600
 
+border_t = 2*s_range
+
 def animate_loop(i):
     ax.clear()
-
+    ax.set_xlim(0,world_x + 2*border_t)
+    ax.set_ylim(0,world_y + 2*border_t)
+    
     for bot in robots:
         bot.update()
     x_bots = [bot.x for bot in robots]
     y_bots = [bot.y for bot in robots]
-    ax.set_xlim(0,world_x)
-    ax.set_ylim(0,world_y)
+
     ax.plot(x_bots, y_bots, 'bo')
     [ax.add_patch(rect) for rect in obstacles] 
 
-robots = [robot(160 + i*25, init_spacing) for i in range(num_bots)]
-
 # Initialize obstacles
 obstacles = [] 
-obs_dims = [(100,100),(100,400),(250,250),(400,100),(400,400)] 
+obs_dims = [(100,100),(100,400),(250,250),(400,100),(400,400)]
+
+obs_dims = [(dims[0] + border_t, dims[1] + border_t) for dims in obs_dims]
 [obstacles.append(patches.Rectangle(dims,obs_width,obs_len,facecolor='r')) for dims in obs_dims]
 obs_dims = [(r._x0, r._y0, r._x0 +r._width, r._y0 + r._height) for r in obstacles]
  
 # Initialize world
-occ_grid = np.zeros((world_y,world_x))
-h_grid = np.zeros_like(occ_grid) # heuristic grid (dist to closest obstacle/wall)
+
+h_grid = np.pad(np.zeros((world_x,world_y)), border_t, mode='constant', constant_values=1)
+occ_grid = np.zeros((2, h_grid.shape[0], h_grid.shape[1]))
+
+for r in obs_dims:
+    h_grid[r[0]:r[2], r[1]:r[3]] = 1 # votes for 0 or 1 by different robots/passes
+
+# Initialize robots
+robots = [robot(150 + border_t + random()*250, 50 + border_t, world=h_grid, occ_grid=occ_grid) for i in range(num_bots)]
 
 # create animation
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-animation = FuncAnimation(fig, func=animate_loop, interval=1000)
+animation = FuncAnimation(fig, func=animate_loop, interval=10)
 
 # start animation
 plt.show()
+
