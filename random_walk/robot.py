ff67ef58b3a2160b7b88561fbe4f2a565ea22ea6
Alec Xu <Alec_Xu@hotmail.com>
2020-12-18 22:20:48 -0800

    initial commit occ_grid update

--- random_walk/robot.py
+++ random_walk/robot.py
@@ -4,6 +4,8 @@
 from math import cos, sin, pi
 from scipy.stats import levy
 from scipy.stats import cauchy
+from scipy.spatial.distance import euclidean
+import numpy as np
 import random
 
 class robot:
@@ -12,19 +14,20 @@ class robot:
     #   creates robot and initializes location to 0, 0
     #
 
-    def __init__(self, x_init = 0, y_init = 0, heading_init = 0, measurement_range = 30.0,
-                 motion_noise = 1.0, measurement_noise = 1.0, step_len = 1.0,
-                 landmarks = [], obstacles = []):
+    def __init__(self, x_init = 0, y_init = 0, heading_init = 0, sense_range = 10,
+                 motion_noise = 0.1, measurement_noise = 1.0, step_len = 1.0,
+                 landmarks = [], world = np.asarray([]), occ_grid = np.asarray([])):
         self.x = x_init
         self.y = y_init
-        self.measurement_range = measurement_range
+        self.sense_range = sense_range
         self.heading = heading_init
         self.motion_noise = motion_noise
         self.measurement_noise = measurement_noise
         self.step_len = step_len
         self.steps_remaining = 0
         self.landmarks = landmarks
-        self.obstacles = obstacles
+        self.world = world
+        self.occ_grid = occ_grid
 
     def rand(self):
         return float(random.random() * 2.0 - 1.0)
@@ -62,12 +65,16 @@ class robot:
         for i in range(len(self.landmarks)):
             dx = self.landmarks[i][0] - self.x + self.rand() * self.measurement_noise
             dy = self.landmarks[i][1] - self.y + self.rand() * self.measurement_noise    
-            if self.measurement_range < 0.0 or abs(dx) + abs(dy) <= self.measurement_range:
+            if self.sense_range < 0.0 or abs(dx) + abs(dy) <= self.sense_range:
                 Z.append([i, dx, dy])
         return Z
 
-    def collision(self):
-        return False
+    def sense_proximity(self):
+        neighborhood = self.world[round(self.x) - self.sense_range:round(self.x) + self.sense_range + 1, 
+                                  round(self.y) - self.sense_range:round(self.y) + self.sense_range + 1]
+        if np.sum(neighborhood) > 0:
+            return True, neighborhood
+        return False, neighborhood
     #
     # print robot location, heading, and num steps
     #
@@ -77,9 +84,15 @@ class robot:
             (self.x, self.y, self.heading, self.steps_remaining)
 
     def update(self):
-        collided = self.collision()
+        # Sense
+        landmarks = self.sense_landmarks()
+        collided, prox = self.sense_proximity()
+
+        # Move
         if(collided):
-            return True
+            self.turn(pi) # turn 180 degrees
+            self.steps_remaining += 3
+            self.step()
         elif self.steps_remaining <= 0:
             self.steps_remaining = int(levy.rvs())
             self.heading += cauchy.rvs()
@@ -90,3 +103,13 @@ class robot:
             self.step()
             self.steps_remaining -= 1
             return True
+        
+        # Record
+        x_lower = round(self.x) - self.sense_range
+        x_higher = round(self.x) + self.sense_range + 1
+        y_lower = round(self.y) - self.sense_range
+        y_higher = round(self.y) + self.sense_range + 1
+        self.occ_grid[1][x_lower:x_higher,y_lower:y_higher] += prox.astype(int)
+        self.occ_grid[0][x_lower:x_higher,y_lower:y_higher] += np.logical_not(prox).astype(int)
+
+        
